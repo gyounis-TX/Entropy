8d93e5705d98fe875711735cfe1c6270779111fa
George Younis <gyounis@gmail.com>
2026-01-17 22:17:10 -0600

    Initial Commit

---
+++ Procedus/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Procedus
+//
+//  Created by George Younis on 1/17/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
